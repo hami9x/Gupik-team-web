7f9e3c0b6d28ad48408d74cd1f61ae66f5efcc60
Nguyễn Thành Hải <phaikawl@gmail.com>
2012-05-25 10:06:06 +0700

    begin implementing the online list, now just set the online, cannot become offline

--- mainpage.py
+++ mainpage.py
@@ -1,13 +1,30 @@
 import webapp2
 from google.appengine.api import users
+from google.appengine.api import channel
+from google.appengine.ext import db
 
 import common
+
+class UserOnline(db.Model):
+    user = db.UserProperty()
+    online = db.BooleanProperty()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
+        online = UserOnline.all().filter("user =", user).get()
+        if not online:
+            model = UserOnline(user=user, online=True)
+            model.put();
+        else:
+            online.online = True
+            online.put()
+        token = channel.create_channel(user.user_id())
+        online_list = db.GqlQuery("SELECT user FROM UserOnline WHERE online = TRUE")
         values = {
                     "user": user,
                     "login_url": users.create_login_url("/"),
+                    "token": token,
+                    "online_list": online_list,
                 }
         self.response.out.write(common.templateRender("main.html", values))
