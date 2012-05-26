452d7dfa199dfa9f772372d1066ab2a4906c17d4
Nguyễn Thành Hải <phaikawl@gmail.com>
2012-05-26 09:13:18 +0700

    small tweaks

--- mainpage.py
+++ mainpage.py
@@ -14,11 +14,13 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            obj = UserOnline.all().filter("user =", user).get()
-            if not obj:
+            q = UserOnline.all().filter("user =", user).get()
+            if not q:
                 model = UserOnline(user=user, online=1)
                 model.put()
-                MyUser(id = user.user_id(), email = user.email()).put()
+            q = MyUser.all().filter("id =", user.user_id()).get()
+            if not q:
+                MyUser(id=user.user_id(), email=user.email()).put()
 
             token = channel.create_channel(user.email())
             online_list = db.GqlQuery("SELECT user FROM UserOnline WHERE online = 1 AND user != :1", user)
