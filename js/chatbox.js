56669b067e69aaa33a89d2688d60c377d043d8b8
Nguyễn Thành Hải <phaikawl@gmail.com>
2012-05-29 10:49:35 +0700

    finally, the chat functionality is now working\!

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,26 +1,26 @@
 //This function takes a string "year:month:day:hour:min" in UTC and converts to local time
 //Returns a string representation of the time
 function toLocalTime(str) {
-    var timeArr = msg.time.split(":");
-    for (var i=0; i<timeArr.length(); i++) {
+    var timeArr = str.split(":");
+    for (var i=0; i<timeArr.length; i++) {
         timeArr[i] = parseInt(timeArr[i]);
     }
     var d = new Date(timeArr[0], timeArr[1], timeArr[2], timeArr[3], timeArr[4], 0, 0);
-    var nd = new Date(d + getTimezoneOffset()*60000);
+    var d2 = new Date();
+    var nd = new Date(d.getTime() - d2.getTimezoneOffset()*60000);
     var year = nd.getFullYear();
-    var month = nd.getMonth()+1;
-    var day = nd.getDay();
+    var month = nd.getMonth();
+    var day = nd.getDate();
     var hour = nd.getHours();
     var min = nd.getMinutes();
-    return year+"/"+month+"/"+day+" "+hour+":"+min;
+    return year+"/"+month+"/"+day+" "+hour+"h"+min;
 }
 
 function chatbox_onMessage(message) {
-    console.log("chatbox_onMessage triggered!");
     var msg = eval("("+message.data+")");
     if (msg.type !== "chatMsg") return;
     var timeStr = toLocalTime(msg.time);
-    $("#result").prepend('<span class="time">['+timeStr+']</span>'+
+    $("#result").prepend('<span class="time">['+timeStr+']</span> '+
                             '<span class="user">'+msg.nickname+'</span>'+
                             ': <span class="pagetext">'+msg.content+'</span>'+
                             '<br/>');
@@ -29,12 +29,12 @@ function chatbox_onMessage(message) {
 registerMessageHandler(chatbox_onMessage);
 
 $(document).ready(function(){
-    $("#chip_chatbox > #do").click(function(){
+    $("#chip_chatbox > #do").click(function(event){
+        event.preventDefault();
+        event.stopPropagation();
         var speed = 'fast';
         var str = $("#chip_chatbox").serialize();
-        $("#result").fadeOut(speed,function(){
-            if (!validate()) return;
-            $.post('/chatbox', str);
-        });
+        if (!validate()) return;
+        $.post('/chatbox', str);
     });
 });
